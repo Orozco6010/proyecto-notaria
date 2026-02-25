80afbd90e81b7773dd54811f1afba2c1376015e1
Orozco6010 <orozcocorrea7217@gmail.com>
2026-02-25 18:02:52 -0500

    modularización de componentes y estilos

---
+++ scripts/main.js
@@ -0,0 +1,26 @@
+const setActiveLink = () => {
+  const currentPath = window.location.pathname;
+
+  const links = document.querySelectorAll("nav a");
+
+  links.forEach((link) => {
+    const linkPath = new URL(link.href).pathname;
+
+    if (currentPath === linkPath) {
+      link.classList.add("activo");
+    }
+  });
+};
+
+const loadComponent = async (elementId, path) => {
+  const res = await fetch(path);
+  const data = await res.text();
+  document.getElementById(elementId).innerHTML = data;
+
+  if (elementId === "header") {
+    setActiveLink();
+  }
+};
+
+loadComponent("header", "/components/header.html");
+loadComponent("footer", "/components/footer.html");
