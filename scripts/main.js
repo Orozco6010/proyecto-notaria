f58d62542217fc51218d4ab83de41e362084b323
Orozco6010 <orozcocorrea7217@gmail.com>
2026-02-28 13:35:51 -0500

    fix header

--- scripts/main.js
+++ scripts/main.js
@@ -1,30 +1,7 @@
-const setActiveLink = () => {
-  const currentPath = window.location.pathname;
-  const isNotLocal = currentPath.split("/").includes("proyecto-notaria");
-  console.log({ isNotLocal });
-  const links = document.querySelectorAll("nav a");
-
-  links.forEach((link) => {
-    const linkPath = new URL(link.href).pathname;
-    if (currentPath === linkPath) {
-      link.classList.add("activo");
-    }
-  });
-};
-
 const loadComponent = async (elementId, path) => {
   const res = await fetch(path);
   const data = await res.text();
   document.getElementById(elementId).innerHTML = data;
-
-  if (elementId === "header") {
-    setActiveLink();
-  }
 };
 
-const basePath = window.location.pathname.includes("/proyecto-notaria/")
-  ? "../"
-  : "";
-
-loadComponent("header", "/components/header.html");
 loadComponent("footer", "/components/footer.html");
