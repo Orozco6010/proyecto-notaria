4439d2ccba7e7281ff04652105fb0210b22ee04f
Orozco6010 <orozcocorrea7217@gmail.com>
2026-02-28 13:38:53 -0500

    script

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,1 @@
-const loadComponent = async (elementId, path) => {
-  const res = await fetch(path);
-  const data = await res.text();
-  document.getElementById(elementId).innerHTML = data;
-};
-
-loadComponent("footer", "/components/footer.html");
+console.log("working");
